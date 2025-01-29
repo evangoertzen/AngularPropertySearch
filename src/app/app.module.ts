6983e4d76136cc4d9dd9244382c6256f325c2d1e
evangoertzen <evan.goertzen@gmail.com>
2025-01-28 22:49:03 -0800

    Upgrade to Angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PropertyViewComponent } from './components/property-view/property-view.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { PropertyPanelComponent } from './components/property-panel/property-panel.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    PropertyViewComponent,
-    PropertyPanelComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        PropertyViewComponent,
+        PropertyPanelComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
