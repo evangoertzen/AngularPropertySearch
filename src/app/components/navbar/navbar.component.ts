08a784327c0102dd547079d5cf168ac56c2ae35b
evangoertzen <evan.goertzen@gmail.com>
2025-02-09 01:04:01 -0800

    Show search form as dropdown on navbar and only show it when on home page

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css',
   standalone: false
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  isHomeRoute = false;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.isHomeRoute = this.router.url === '/';
+    });
+  }
 
 }
