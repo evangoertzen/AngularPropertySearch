308b08cdb2319d860b92ab35aa92d93cbacd7ec3
evangoertzen <evan.goertzen@gmail.com>
2025-03-19 17:31:47 -0400

    Use commas to make large numbers understandable for numerical input.

--- src/app/components/analysis/shared/numerical-input/numerical-input.component.ts
+++ src/app/components/analysis/shared/numerical-input/numerical-input.component.ts
@@ -1,40 +1,28 @@
-import { NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormatService } from 'src/app/services/format-service/format.service';
 
 @Component({
   selector: 'app-numerical-input [modelField]',
   templateUrl: './numerical-input.component.html',
-  styleUrl: './numerical-input.component.css',
-  standalone: false
+  styleUrls: ['./numerical-input.component.css'],
+  standalone: false,
 })
 export class NumericalInputComponent {
-
-  @Input()
-  modelField!: number;
-
-  @Output()
-  modelFieldChange = new EventEmitter<number>();
-
-  @Input()
-  numType: 'PERCENT' | 'DOLLARS' | null = null;
-
-  @Input()
-  placeHolder: string = '';
-
-  @Input()
-  disabled: boolean = false;
-
-  @Input()
-  colorMode: string = 'default'; // 'GP-RM' color mode to make green when positive and red when negative
+  
+  @Input() modelField!: number;
+  @Output() modelFieldChange = new EventEmitter<number>();
+  
+  @Input() numType: 'PERCENT' | 'DOLLARS' | null = null;
+  @Input() placeHolder: string = '';
+  @Input() disabled: boolean = false;
+  @Input() colorMode: string = 'default'; // 'GP-RM' color mode to make green when positive and red when negative
+  
+  constructor(public formatService: FormatService) {}
 
   onInputChange(event: Event) {
-    const newValue = (event.target as HTMLInputElement).valueAsNumber;
-    this.modelFieldChange.emit(newValue); // Notify parent of new value
-  }
+    const newValue = (event.target as HTMLInputElement).value.replace(/,/g, ''); // Remove commas
 
-  formattedValue(val: number): string {
-    return val % 1 === 0
-        ? val.toFixed(0) 
-        : val.toFixed(2);
+    if(newValue != '')
+    this.modelFieldChange.emit(parseFloat(newValue)); // Notify parent of new value
   }
 }
--- src/app/services/format-service/format.service.ts
+++ src/app/services/format-service/format.service.ts
@@ -8,11 +8,14 @@ export class FormatService {
   constructor() { }
 
   formatWithCommas(input: number | string): string {
-
-    // convert to num
     const number = typeof input === 'string' ? parseFloat(input) : input;
-
-    // add commas
     return number.toLocaleString();
+  }
+
+  // use to format numeric inputs
+  numericValFormatted(val: number){
+    return val % 1 === 0
+        ? this.formatWithCommas(val.toFixed(0))
+        : this.formatWithCommas(val.toFixed(2));
   }
 }
