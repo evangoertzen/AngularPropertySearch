8cf457792d747e6b6248e4d570d8a98b88297360
evangoertzen <evan.goertzen@gmail.com>
2025-04-07 18:52:32 -0700

    Create analysis service to hold variables shared on analysis page.

---
+++ Angular/src/app/services/analysis-service/analysis.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CalculatorService } from '../calculator-service/calculator-service.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnalysisService {
+
+}
